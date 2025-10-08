3757405d80017f38aa54091d5f2a2cbd1913f705
Delilah Clarke <dylanjclarke18@gmail.com>
2025-10-08 18:54:41 -0400

    Updated tokenize function, added scan_token function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-use crate::parser::scan_token;
+// use crate::parser::scan_token;
+use crate::parser::tokenize;
+use std::fs::File;
+use std::io::{self, Read};
+use std::path::Path;
 
 mod parser;
 
-fn main() {
-    scan_token("turn{");
+fn main() -> io::Result<()>{
+    let path = Path::new("/home/delilah/Documents/Projects/rover/src/test.txt");
+    tokenize("hello world");
+    Ok(())
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,7 @@
-
-enum Vocab{
-    
-}
+use std::fs::{read_to_string, File};
+use std::io::{self, Read};
+use std::path::Path;
+use std::str::Chars;
 
 pub enum Token {
     Turn,
@@ -16,16 +16,63 @@ pub enum Token {
     Char(char),
 } 
 
-pub struct ParseError;
+pub fn read_file (file_path: &Path) -> String {
+    let mut file = File::open(file_path).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents);
+    contents
+}
 
-pub fn scan_token(input: &str) -> Result<Option<Token>, ParseError> {
-    let mut words = input
-        .split_inclusive(|c: char| c.is_whitespace() || c == '{' || c == '}')
-        .map(|c| c.trim());
+pub fn scan_token (chars: &mut Chars) -> Option<Token> {
+    match chars.next() {
+        Some(c) if c.is_alphabetic() => {
+            let mut buf = String::new();
+            buf.push(c);
+            while let Some(x) = chars.next().filter(|c| c.is_alphabetic()){
+                buf.push(x);
+            }
+            match buf.as_str() {
+                "turn" => Some(Token::Turn),
+                "move" => Some(Token::Move),
+                "if" => Some(Token::If),
+                "else" => Some(Token::Else),
+                "repeat" => Some(Token::Repeat),
+                "looking" => Some(Token::Looking),
+                _ => panic!(),
+            }
+        },
+        Some(c) if c.is_ascii_digit() => {
+            let mut buf = String::new();
+            buf.push(c);
+            while let Some(x) = chars.next().filter(|c| c.is_ascii_digit()){
+                buf.push(x);
+            }
+            Some(Token::Int(buf.parse().unwrap()))
+        }
+        _ => todo!(),
+    }
+}
 
-    let x: Vec<_> = words.collect();
-    println!("{:?}", x);
-    Err(ParseError)
+
+pub fn tokenize (s: &str) -> Vec<Token> {
+    let mut chars = s.chars();
+    let mut tokens = Vec::new();
+    while let Some(t) = scan_token(&mut chars){
+        tokens.push(t);
+    }
+    tokens
+
+}
+// pub struct ParseError;
+
+// pub fn scan_token(input: &str) -> Result<Option<Token>, ParseError> {
+//     let mut words = input
+//         .split_inclusive(|c: char| c.is_whitespace() || c == '{' || c == '}')
+//         .map(|c| c.trim());
+
+//     let x: Vec<_> = words.collect();
+//     println!("{:?}", x);
+//     Err(ParseError)
 
     // match words.next() {
     //     Some("turn") => {
@@ -36,4 +83,5 @@ pub fn scan_token(input: &str) -> Result<Option<Token>, ParseError> {
     //     }
     //     None => Ok(None)
     // }
-}
+// }
+
